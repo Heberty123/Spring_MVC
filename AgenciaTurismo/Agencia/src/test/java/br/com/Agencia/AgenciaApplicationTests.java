cfa10a3e2c8a10e85fcb98d869de35c3fc2a9c4d
Heberty <hebertyprado22005@gmail.com>
2022-02-18 21:58:18 -0300

    att

---
+++ AgenciaTurismo/Agencia/src/test/java/br/com/Agencia/AgenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
