27008295e734a69fe2d57ab4977c5048726c6f33
Heberty <hebertyprado22005@gmail.com>
2022-03-09 16:08:56 -0300

    att

--- AgenciaTurismo/Agencia/src/main/java/br/com/Agencia/Controllers/ContinenteController.java
+++ AgenciaTurismo/Agencia/src/main/java/br/com/Agencia/Controllers/ContinenteController.java
@@ -1,9 +1,10 @@
 package br.com.Agencia.Controllers;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.Agencia.Models.Continente;
 import br.com.Agencia.Repositories.ContinenteRepository;
@@ -11,9 +12,28 @@ import br.com.Agencia.Repositories.ContinenteRepository;
 @Controller
 @RequestMapping(value = "/Continente")
 public class ContinenteController {
+
+	/*
+	static {
+		ContinenteRepository continenteRepository = null;
+		
+		continenteRepository.save(new Continente("Europa"));
+		continenteRepository.save(new Continente("America"));
+		continenteRepository.save(new Continente("Ásia"));
+		continenteRepository.save(new Continente("Oceania"));
+		continenteRepository.save(new Continente("África"));
+	}
+	*/
 	
-	ContinenteRepository continenteRepository;
+	@GetMapping("/index")
+	public ModelAndView continentes() {
+		ModelAndView mv = new ModelAndView("Continentes/");
+		
+		
+		return mv;
+	}
 	
 	
+
 	
 }
