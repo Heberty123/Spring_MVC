4f0597675847d7bde8a96d90b978aa0f48c8880c
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-04-05 15:21:23 -0300

    att

---
+++ Perfumaria/Perfumaria/src/test/java/Amelia/Perfumaria/PerfumariaApplicationTests.java
@@ -0,0 +1,13 @@
+package Amelia.Perfumaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
